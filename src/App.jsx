0b56b421923dc06d6f9caa377676ab103fc7139d
Noah Palin <noah.m.palin@gmail.com>
2023-01-05 15:45:13 -0500

    Setup react router 6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  
-  
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+
 }
 
 export default App
