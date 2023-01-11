6bf6965d5f1e1456ee2516827019408b9b8d7b71
Noah Palin <noah.m.palin@gmail.com>
2023-01-11 11:35:52 -0500

    Add nav options for each page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import './styles/App.css'
+import { Link, Routes, Route } from 'react-router-dom'
 import About from './routes/about'
 import Home from './routes/home'
 import Professional from './routes/professional'
@@ -9,11 +9,19 @@ import Personal from './routes/personal'
 function App() {
   return (
     <>
+    <nav>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/professional'>Professional</Link>
+        <Link to='/personal'>Personal</Link>
+      </ul>
+    </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/professional" element={<Professional />} />
-        <Route path="/personal" element={<Personal />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/professional' element={<Professional />} />
+        <Route path='/personal' element={<Personal />} />
       </Routes>
     </>
   )
