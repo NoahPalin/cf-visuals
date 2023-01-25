7816d4c7cd0a6e63f2a60a67ff8c5361379ac303
Noah Palin <noah.m.palin@gmail.com>
2023-01-25 16:45:50 -0500

    Make navbar switch to a dropdown when screen width in small.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, Routes, Route } from 'react-router-dom'
 import '../styles/nav.css'
 import cameraLogo from './images/camera-2.png'
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavDropdown from './nav-dropdown';
 
 function NavMain() {
+
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  console.log(dimensions);
+  const handleResize = () => {
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleResize, false);
+  }, []);
+
+
   return (
     <>
       <div className='nav'>
-        <img className='logo' src={cameraLogo} alt='CF-Visuals logo.'/>
+        <img className='logo' src={cameraLogo} alt='CF-Visuals logo.' />
         <div className='title'>CF Visuals</div>
         <div className='nav-links'>
-          <Link className='link' to='/'>Home</Link>
-          <Link className='link' to='/about'>About</Link>
-          <Link className='link' to='/professional'>Professional</Link>
-          <Link className='link' to='/personal'>Personal</Link>
+          {dimensions.width < 400 ? <NavDropdown /> :
+            <>
+              <Link className='link' to='/'>Home</Link>
+              <Link className='link' to='/about'>About</Link>
+              <Link className='link' to='/professional'>Professional</Link>
+              <Link className='link' to='/personal'>Personal</Link>
+            </>
+          }
         </div>
       </div>
     </>
