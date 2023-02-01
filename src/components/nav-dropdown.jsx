cc3b7ee064c6af7a282db15146fc38dbfbd78a34
Noah Palin <noah.m.palin@gmail.com>
2023-02-01 12:21:34 -0500

    Change dropdown nav buttons to navigate to correct pages.

--- src/components/nav-dropdown.jsx
+++ src/components/nav-dropdown.jsx
@@ -2,17 +2,34 @@ import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import '../styles/nav-dropdown.css'
+import { Link, Routes, Route } from 'react-router-dom'
 
 function NavDropdown() {
   return (
     <div>
       <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    </DropdownButton>
+
+        <Dropdown.Item>
+          <Link className='dropdown-link' to='/'>Home</Link>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <Link className='dropdown-link' to='/about'>About</Link>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <Link className='dropdown-link' to='/professional'>Professional</Link>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <Link className='dropdown-link' to='/personal'>Personal</Link>
+        </Dropdown.Item>
+
+      </DropdownButton>
     </div>
   )
 }
-
+// <Link className='link' to='/about'>About</Link>
+// <Link className='link' to='/professional'>Professional</Link>
+// <Link className='link' to='/personal'>Personal</Link>
 export default NavDropdown;
