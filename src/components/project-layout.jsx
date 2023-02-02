06de53d21695781c98ded6f89ebc2da2fd4c8442
Noah Palin <noah.m.palin@gmail.com>
2023-02-02 11:53:52 -0500

    Setup new project layout component.

---
+++ src/components/project-layout.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import '../styles/project-layout.css'
+
+function ProjectLayout() {
+
+  return (
+    <div className='grid-container'>
+     
+    </div>
+  )
+}
+
+export default ProjectLayout;
