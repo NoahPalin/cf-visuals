23318c604063788d39b2b7f05968e7b97963fae8
Noah Palin <noah.m.palin@gmail.com>
2023-02-07 15:10:01 -0500

    Adjust layout to be more simple.

--- src/components/project-layout.jsx
+++ src/components/project-layout.jsx
@@ -2,31 +2,33 @@ import '../styles/project-layout.css'
 import liam1 from './images/home-images/DSC_0044.jpg';
 import liam2 from './images/home-images/DSC_0052.jpg';
 import liam3 from './images/home-images/DSC_0080.jpg';
+import liam4 from './images/home-images/DSC_0170.jpg';
+import liam5 from './images/home-images/DSC_0200.jpg';
+
 
 
 function ProjectLayout() {
 
   return (
-    <div className='grid-container'>
-      <div className='grid-item grid-item-1'>
-        image1
+    <>
+      <div className='grid-container'>
+        <div className='grid-item grid-item-1'>
+          <img src={liam1} alt='Liam' />
+        </div>
+        <div className='grid-item grid-item-2'>
+          <img src={liam2} alt='Liam' />
+        </div>
+        <div className='grid-item grid-item-3'>
+          <img src={liam3} alt='Liam' />
+        </div>
+        <div className='grid-item grid-item-4'>
+          <img src={liam4} alt='Liam' />
+        </div>
+        <div className='grid-item grid-item-5'>
+          <img src={liam5} alt='Liam' />
+        </div>
       </div>
-      <div className='grid-item grid-item-2'>
-        paragraph1
-      </div>
-      <div className='grid-item grid-item-3'>
-        image2
-      </div>
-      <div className='grid-item grid-item-4'>
-        paragraph2
-      </div>
-      <div className='grid-item grid-item-5'>
-        image3
-      </div>
-      <div className='grid-item grid-item-6'>
-        paragraph3
-      </div>
-    </div>
+    </>
   )
 }
 
