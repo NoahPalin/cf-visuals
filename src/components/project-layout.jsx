90a460cbaa73039d2c9333a4f8e29220f5e66392
Noah Palin <noah.m.palin@gmail.com>
2023-02-07 12:53:52 -0500

    Use grid-area.

--- src/components/project-layout.jsx
+++ src/components/project-layout.jsx
@@ -4,9 +4,12 @@ function ProjectLayout() {
 
   return (
     <div className='grid-container'>
-      <div className='grid-item grid-item-1'>Hi</div>
-      <div className='grid-item grid-item-2'>Hello</div>
-      <div className='grid-item grid-item-3'>Hey</div>
+      <div className='grid-item grid-item-1'>image1</div>
+      <div className='grid-item grid-item-2'>paragraph1</div>
+      <div className='grid-item grid-item-3'>image2</div>
+      <div className='grid-item grid-item-1'>paragraph2</div>
+      <div className='grid-item grid-item-2'>image3</div>
+      <div className='grid-item grid-item-3'>paragraph3</div>
     </div>
   )
 }
