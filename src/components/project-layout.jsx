15224e586e90a1a00e92db02b8ccaef152ddf5fd
Noah Palin <noah.m.palin@gmail.com>
2023-02-07 13:04:05 -0500

    Create a staggered layout.

--- src/components/project-layout.jsx
+++ src/components/project-layout.jsx
@@ -7,9 +7,9 @@ function ProjectLayout() {
       <div className='grid-item grid-item-1'>image1</div>
       <div className='grid-item grid-item-2'>paragraph1</div>
       <div className='grid-item grid-item-3'>image2</div>
-      <div className='grid-item grid-item-1'>paragraph2</div>
-      <div className='grid-item grid-item-2'>image3</div>
-      <div className='grid-item grid-item-3'>paragraph3</div>
+      <div className='grid-item grid-item-4'>paragraph2</div>
+      <div className='grid-item grid-item-5'>image3</div>
+      <div className='grid-item grid-item-6'>paragraph3</div>
     </div>
   )
 }
