05491947e4455bc52d5de901927f971cd1a996e6
Noah Palin <noah.m.palin@gmail.com>
2023-02-09 12:28:58 -0500

    More images.

--- src/components/all-projects-layout.jsx
+++ src/components/all-projects-layout.jsx
@@ -9,6 +9,28 @@ import liam7 from './images/photoshoots/liam-photoshoot/DSC_0622.jpg';
 import liam8 from './images/photoshoots/liam-photoshoot/DSC_0635.jpg';
 import liam9 from './images/photoshoots/liam-photoshoot/DSC_0350.jpg';
 
+import bliss1 from './images/photoshoots/bliss-photoshoot/DSC_0031.jpg';
+import bliss2 from './images/photoshoots/bliss-photoshoot/DSC_0033.jpg';
+import bliss3 from './images/photoshoots/bliss-photoshoot/DSC_0050.jpg';
+import bliss4 from './images/photoshoots/bliss-photoshoot/DSC_0069.jpg';
+import bliss5 from './images/photoshoots/bliss-photoshoot/DSC_0076.jpg';
+import bliss6 from './images/photoshoots/bliss-photoshoot/DSC_0122.jpg';
+import bliss7 from './images/photoshoots/bliss-photoshoot/DSC_0151.jpg';
+import bliss8 from './images/photoshoots/bliss-photoshoot/DSC_0303.jpg';
+import bliss9 from './images/photoshoots/bliss-photoshoot/DSC_0331.jpg';
+
+import linh1 from './images/photoshoots/linh-photoshoot/DSC_0088.jpg';
+import linh2 from './images/photoshoots/linh-photoshoot/DSC_0089.jpg';
+import linh3 from './images/photoshoots/linh-photoshoot/DSC_0101.jpg';
+import linh4 from './images/photoshoots/linh-photoshoot/DSC_0106.jpg';
+import linh5 from './images/photoshoots/linh-photoshoot/DSC_0117.jpg';
+import linh6 from './images/photoshoots/linh-photoshoot/DSC_0118.jpg';
+import linh7 from './images/photoshoots/linh-photoshoot/DSC_0132.jpg';
+import linh8 from './images/photoshoots/linh-photoshoot/DSC_0136.jpg';
+import linh9 from './images/photoshoots/linh-photoshoot/DSC_0156.jpg';
+
+
+
 
 
 function AllProjectsLayout() {
@@ -33,6 +55,26 @@ function AllProjectsLayout() {
         <img className='grid-item-main grid-item-7-main' src={liam7} alt='Liam' />
         <img className='grid-item-main grid-item-8-main' src={liam8} alt='Liam' />
         <img className='grid-item-main grid-item-9-main' src={liam9} alt='Liam' />
+
+        <img className='grid-item-main grid-item-10-main' src={bliss1} alt='Bliss' />
+        <img className='grid-item-main grid-item-11-main' src={bliss2} alt='Bliss' />
+        <img className='grid-item-main grid-item-12-main' src={bliss3} alt='Bliss' />
+        <img className='grid-item-main grid-item-13-main' src={bliss4} alt='Bliss' />
+        <img className='grid-item-main grid-item-14-main' src={bliss5} alt='Bliss' />
+        <img className='grid-item-main grid-item-15-main' src={bliss6} alt='Bliss' />
+        <img className='grid-item-main grid-item-16-main' src={bliss7} alt='Bliss' />
+        <img className='grid-item-main grid-item-17-main' src={bliss8} alt='Bliss' />
+        <img className='grid-item-main grid-item-18-main' src={bliss9} alt='Bliss' />
+
+        <img className='grid-item-main grid-item-19-main' src={linh1} alt='Linh' />
+        <img className='grid-item-main grid-item-20-main' src={linh2} alt='Linh' />
+        <img className='grid-item-main grid-item-21-main' src={linh3} alt='Linh' />
+        <img className='grid-item-main grid-item-22-main' src={linh4} alt='Linh' />
+        <img className='grid-item-main grid-item-23-main' src={linh5} alt='Linh' />
+        <img className='grid-item-main grid-item-24-main' src={linh6} alt='Linh' />
+        <img className='grid-item-main grid-item-25-main' src={linh7} alt='Linh' />
+        <img className='grid-item-main grid-item-26-main' src={linh8} alt='Linh' />
+        <img className='grid-item-main grid-item-27-main' src={linh9} alt='Linh' />
       </div>
     </>
   )
