3df7439cce3bb58e46fdd66967b492c924b18dca
Noah Palin <noah.m.palin@gmail.com>
2023-02-20 10:24:04 -0500

    Add more text to personal.jsx.

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -8,7 +8,7 @@ function Personal() {
         <div className='text-area'>
           <div className='page-title'>Liam Canney</div>
           <div className='text-area-paragraph'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl condimentum id venenatis. Purus semper eget duis at tellus at urna condimentum. Pharetra massa massa ultricies mi quis. Euismod in pellentesque massa placerat duis ultricies sed.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl condimentum id venenatis. Purus semper eget duis at tellus at urna condimentum. Pharetra massa massa ultricies mi quis. Euismod in pellentesque massa placerat duis ultricies sed. Euismod in pellentesque massa placerat duis ultricies sed.
           </div>
         </div>
       </div>
