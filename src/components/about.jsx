0756d05d927339e8df25d8ebd9ea6e5b9bb1d5d2
Noah Palin <noah.m.palin@gmail.com>
2023-02-23 16:10:25 -0500

    Update to about.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,7 +8,7 @@ function About() {
       <img className='caira-photo' src={caira} alt='Caira' />
       <div className='text-box'>
         <h1>Caira Catney-Forbes</h1>
-        <p>Hi, I'm Caira. I'm a photographer and web developer based in the UK. I'm currently studying for a BSc in Computer Science at the University of Bristol. I'm passionate about photography and web development, and I'm always looking for new opportunities to learn and grow.</p>
+        <p>Hi, I'm Caira. I'm a photographer and web developer based in the UK. I'm currently studying for a BSc in Computer Science at the University of Bristol. I'm passionate about photography and web development, and I'm always looking for new opportunities to learn and grow (generated automatically).</p>
       </div>
     </div>
     </>
